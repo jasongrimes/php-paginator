d76e9bd45d9b65c1e7e9c056f91551d955871bde
Jason Grimes <jason@grimesit.com>
2014-09-27 23:00:42 -0400

    Initial commit.

---
+++ src/JasonGrimes/Paginator.php
@@ -0,0 +1,305 @@
+<?php
+
+namespace JasonGrimes;
+
+class Paginator
+{
+    const NUM_PLACEHOLDER = '(:num)';
+
+    protected $numItems;
+    protected $numPages;
+    protected $itemsPerPage;
+    protected $currentPage;
+    protected $urlPattern;
+    protected $maxPagesToShow = 10;
+
+    /**
+     * @param int $numItems The total number of items.
+     * @param int $itemsPerPage The number of items per page.
+     * @param int $currentPage The current page number.
+     * @param string $urlPattern A URL for each page, with (:num) as a placeholder for the page number. Ex. '/foo/page/(:num)'
+     */
+    public function __construct($numItems, $itemsPerPage, $currentPage, $urlPattern = '')
+    {
+        $this->numItems = $numItems;
+        $this->itemsPerPage = $itemsPerPage;
+        $this->currentPage = $currentPage;
+        $this->urlPattern = $urlPattern;
+
+        $this->updateNumPages();
+    }
+
+    protected function updateNumPages()
+    {
+        $this->numPages = (int) ceil($this->numItems/$this->itemsPerPage);
+    }
+
+    /**
+     * @param int $maxPagesToShow
+     * @throws \InvalidArgumentException if $maxPagesToShow is less than 3.
+     */
+    public function setMaxPagesToShow($maxPagesToShow)
+    {
+        if ($maxPagesToShow < 3) {
+            throw new \InvalidArgumentException('maxPagesToShow cannot be less than 3.');
+        }
+        $this->maxPagesToShow = $maxPagesToShow;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMaxPagesToShow()
+    {
+        return $this->maxPagesToShow;
+    }
+
+    /**
+     * @param int $currentPage
+     */
+    public function setCurrentPage($currentPage)
+    {
+        $this->currentPage = $currentPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentPage()
+    {
+        return $this->currentPage;
+    }
+
+    /**
+     * @param int $itemsPerPage
+     */
+    public function setItemsPerPage($itemsPerPage)
+    {
+        $this->itemsPerPage = $itemsPerPage;
+        $this->updateNumPages();
+    }
+
+    /**
+     * @return int
+     */
+    public function getItemsPerPage()
+    {
+        return $this->itemsPerPage;
+    }
+
+    /**
+     * @param int $numItems
+     */
+    public function setNumItems($numItems)
+    {
+        $this->numItems = $numItems;
+        $this->updateNumPages();
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumItems()
+    {
+        return $this->numItems;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+
+    /**
+     * @param string $urlPattern
+     */
+    public function setUrlPattern($urlPattern)
+    {
+        $this->urlPattern = $urlPattern;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrlPattern()
+    {
+        return $this->urlPattern;
+    }
+
+    /**
+     * @param int $pageNum
+     * @return string
+     */
+    public function getPageUrl($pageNum)
+    {
+        return str_replace(self::NUM_PLACEHOLDER, $pageNum, $this->urlPattern);
+    }
+
+    public function getNextPage()
+    {
+        if ($this->currentPage < $this->numPages) {
+            return $this->currentPage + 1;
+        }
+
+        return null;
+    }
+
+    public function getPrevPage()
+    {
+        if ($this->currentPage > 1) {
+            return $this->currentPage - 1;
+        }
+
+        return null;
+    }
+
+    public function getNextUrl()
+    {
+        if (!$this->getNextPage()) {
+            return null;
+        }
+
+        return $this->getPageUrl($this->getNextPage());
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPrevUrl()
+    {
+        if (!$this->getPrevPage()) {
+            return null;
+        }
+
+        return $this->getPageUrl($this->getPrevPage());
+    }
+
+    /**
+     * Get an array of paginated page data.
+     *
+     * Example:
+     * array(
+     *     array ('num' => 1,     'url' => '/example/page/1',  'isCurrent' => false),
+     *     array ('num' => '...', 'url' => NULL,               'isCurrent' => false),
+     *     array ('num' => 3,     'url' => '/example/page/3',  'isCurrent' => false),
+     *     array ('num' => 4,     'url' => '/example/page/4',  'isCurrent' => true ),
+     *     array ('num' => 5,     'url' => '/example/page/5',  'isCurrent' => false),
+     *     array ('num' => '...', 'url' => NULL,               'isCurrent' => false),
+     *     array ('num' => 10,    'url' => '/example/page/10', 'isCurrent' => false),
+     * )
+     *
+     * @return array
+     */
+    public function getPages()
+    {
+        $pages = array();
+
+        if ($this->numPages <= 1) {
+            return array();
+        }
+
+        if ($this->numPages <= $this->maxPagesToShow) {
+            for ($i = 1; $i <= $this->numPages; $i++) {
+                $pages[] = $this->createPage($i, $i == $this->currentPage);
+            }
+        } else {
+
+            // Determine the sliding range, centered around the current page.
+            $numAdjacents = (int) floor(($this->maxPagesToShow - 3) / 2);
+
+            if ($this->currentPage + $numAdjacents > $this->numPages) {
+                $slidingStart = $this->numPages - $this->maxPagesToShow + 2;
+            } else {
+                $slidingStart = $this->currentPage - $numAdjacents;
+            }
+            if ($slidingStart < 2) $slidingStart = 2;
+
+            $slidingEnd = $slidingStart + $this->maxPagesToShow - 3;
+            if ($slidingEnd >= $this->numPages) $slidingEnd = $this->numPages - 1;
+
+            // Build the list of pages.
+            $pages[] = $this->createPage(1, $this->currentPage == 1);
+            if ($slidingStart > 2) {
+                $pages[] = $this->createPageEllipsis();
+            }
+            for ($i = $slidingStart; $i <= $slidingEnd; $i++) {
+                $pages[] = $this->createPage($i, $i == $this->currentPage);
+            }
+            if ($slidingEnd < $this->numPages - 1) {
+                $pages[] = $this->createPageEllipsis();
+            }
+            $pages[] = $this->createPage($this->numPages, $this->currentPage == $this->numPages);
+        }
+
+
+        return $pages;
+    }
+
+
+    /**
+     * Create a page data structure.
+     *
+     * @param int $pageNum
+     * @param bool $isCurrent
+     * @return Array
+     */
+    protected function createPage($pageNum, $isCurrent = false)
+    {
+        return array(
+            'num' => $pageNum,
+            'url' => $this->getPageUrl($pageNum),
+            'isCurrent' => $isCurrent,
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function createPageEllipsis()
+    {
+        return array(
+            'num' => '...',
+            'url' => null,
+            'isCurrent' => false,
+        );
+    }
+
+    /**
+     * Render an HTML pagination control.
+     *
+     * @return string
+     */
+    public function toHtml()
+    {
+        if ($this->numPages <= 1) {
+            return '';
+        }
+
+        $html = '<ul class="pagination">';
+        if ($this->getPrevUrl()) {
+            $html .= '<li><a href="{{ paginator.prevUrl }}">&laquo; Previous</a></li>';
+        }
+
+        foreach ($this->getPages() as $page) {
+            if ($page['url']) {
+                $html .= '<li' . ($page['isCurrent'] ? ' class="active"' : '') . '><a href="' . $page['url'] . '">' . $page['num'] . '</a></li>';
+            } else {
+                $html .= '<li class="disabled"><span>' . $page['num'] . '</span></li>';
+            }
+        }
+
+        if ($this->getNextUrl()) {
+            $html .= '<li><a href="' . $this->getNextUrl() . '">Next &raquo;</a></li>';
+        }
+        $html .= '</ul>';
+
+        return $html;
+    }
+
+    public function __toString()
+    {
+        return $this->toHtml();
+    }
+}
---
+++ tests/JasonGrimes/Tests/PaginatorTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace JasonGrimes\Tests;
+
+use JasonGrimes\Paginator;
+
+class PaginatorTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Paginator */
+    protected $paginator;
+
+    public function setUp()
+    {
+        $numItems = 100;
+        $itemsPerPage = 10;
+        $currentPage = 5;
+        $urlPattern = '/example/page(:num)';
+
+        $this->paginator = new Paginator($numItems, $itemsPerPage, $currentPage, $urlPattern);
+    }
+
+    public function testGetNextPage()
+    {
+        $this->paginator->setCurrentPage(1);
+        $this->assertEquals(2, $this->paginator->getNextPage());
+
+        // If we're on the last page, getNextPage() returns null.
+        $this->paginator->setCurrentPage($this->paginator->getNumPages());
+        $this->assertNull($this->paginator->getNextPage());
+    }
+
+    public function testGetPrevPage()
+    {
+        $this->paginator->setCurrentPage(2);
+        $this->assertEquals(1, $this->paginator->getPrevPage());
+
+        // If we're on the first page, getPrevPage() returns null.
+        $this->paginator->setCurrentPage(1);
+        $this->assertNull($this->paginator->getPrevPage());
+    }
+
+    public function testGetNextUrl()
+    {
+        $this->paginator->setCurrentPage(1);
+        $this->paginator->setUrlPattern('/example/page(:num)');
+        $this->assertEquals('/example/page2', $this->paginator->getNextUrl());
+
+        // Returns null if on the last page.
+        $this->paginator->setCurrentPage($this->paginator->getNumPages());
+        $this->assertNull($this->paginator->getNextUrl());
+    }
+
+    public function testGetPrevUrl()
+    {
+        $this->paginator->setCurrentPage(2);
+        $this->paginator->setUrlPattern('/example/page(:num)');
+        $this->assertEquals('/example/page1', $this->paginator->getPrevUrl());
+
+        // Returns null if on the first page.
+        $this->paginator->setCurrentPage(1);
+        $this->assertNull($this->paginator->getPrevUrl());
+    }
+
+    public function getTestData()
+    {
+        return array(
+            // num pages, current page, max pages to show, expected pagination
+            array(13, 2, 5, array(1, 2, 3, 4, '...', 13)),
+            array(13, 4, 5, array(1, '...', 3, 4, 5, '...', 13)),
+            array(13, 5, 5, array(1, '...', 4, 5, 6, '...', 13)),
+            array(13, 11, 5, array(1, '...', 10, 11, 12, 13)),
+            array(13, 10, 5, array(1, '...', 9, 10, 11, '...', 13)),
+            array(20, 1, 10, array(1, 2, 3, 4, 5, 6, 7, 8, 9, '...', 20)),
+            array(20, 2, 10, array(1, 2, 3, 4, 5, 6, 7, 8, 9, '...', 20)),
+            array(20, 20, 10, array(1, '...', 12, 13, 14, 15, 16, 17, 18, 19, 20)),
+            array(20, 19, 10, array(1, '...', 12, 13, 14, 15, 16, 17, 18, 19, 20)),
+            array(20, 10, 10, array(1, '...', 7, 8, 9, 10, 11, 12, 13, 14, '...', 20)),
+            array(20, 9, 10, array(1, '...', 6, 7, 8, 9, 10, 11, 12, 13, '...', 20)),
+            array(5, 3, 10, array(1, 2, 3, 4, 5)),
+            array(1, 1, 10, array()), // No pagination if there's only one page.
+            array(20, 5, 3, array(1, '...', 5, '...', 20)),
+        );
+    }
+
+    /**
+     * @dataProvider getTestData
+     */
+    public function testGetPages($numPages, $currentPage, $maxPages, $expected)
+    {
+        $paginator = new Paginator($numPages, 1, $currentPage);
+        $paginator->setMaxPagesToShow($maxPages);
+
+        $pages = $paginator->getPages();
+        $pageNums = array_map(function($page) { return $page['num']; }, $pages);
+
+        $this->assertEquals($expected, $pageNums);
+    }
+
+}
